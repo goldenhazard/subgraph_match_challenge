60bded26a8afbc257178e50af7ede8dc72e37ff4
Wonseok1017 <dnjstjr1017@gmail.com>
2021-05-26 00:14:03 +0900

    ver 210526

--- src/backtrack.cc
+++ src/backtrack.cc
@@ -23,10 +23,9 @@ void Backtrack::PrintAllMatches(const Graph &data, const Graph &query,
     std::cout << "t " << query.GetNumVertices() << "\n";
 
     // implement your code here.
+    query_size = query.GetNumVertices();
     visited_u.resize(query.GetNumVertices());
     visited_v.resize(data.GetNumVertices());
-    extendable_u.resize(query.GetNumVertices());
-    extendable_v.resize(data.GetNumVertices());
 
     std::vector<VertexPair> matchedVertex;
     DoBacktrack(data, query, cs, matchedVertex);
@@ -61,8 +60,8 @@ void PrintEmbedding(std::vector<VertexPair> partial_embedding){
 
 void Backtrack::DoBacktrack(const Graph& data, const Graph& query, const CandidateSet& cs, 
                             std::vector<VertexPair>& partial_embedding){
-    //PrintEmbedding(partial_embedding);
-    if(embedding_number >= 100000) return;
+    PrintEmbedding(partial_embedding);
+    //if(embedding_number >= 100000) return;
     if(partial_embedding.size() == query.GetNumVertices()){
         //Check(data, query, cs, partial_embedding);
         PrintPath(partial_embedding);
@@ -72,135 +71,121 @@ void Backtrack::DoBacktrack(const Graph& data, const Graph& query, const Candida
     else if(partial_embedding.empty()){
         Vertex root = FindRoot(query, cs);
         for(size_t idx = 0; idx < cs.GetCandidateSize(root); idx++){
-            Vertex next_vertex = cs.GetCandidate(root, idx);
-            partial_embedding.push_back(std::make_pair(root, next_vertex));
-            visited_v[next_vertex] = true; visited_u[root] = true;
+            Vertex v_next = cs.GetCandidate(root, idx);
+            partial_embedding.push_back(std::make_pair(root, v_next));
+            visited_v[v_next] = true; visited_u[root] = true;
+            pushUV(partial_embedding.size(), root, v_next, data, query, cs);
             DoBacktrack(data, query, cs, partial_embedding);
+            popUV(root, query);
             partial_embedding.pop_back();
-            visited_v[next_vertex] = false; visited_u[root] = false;
+            visited_v[v_next] = false; visited_u[root] = false;
         }
     }
 
     else{
-        Cmu cmu_next;
-        cmu_next = FindNextVertex(data, query, cs, partial_embedding, cmu_next);
-        if(cmu_next.second.empty()) return;
-        Vertex u_next = cmu_next.first;
-
-        for(Vertex v_next: cmu_next.second){
+        Vertex u_next = FindNextVertex(partial_embedding.size());
+        if(u_next == 99999) return;
+        for(Vertex v_next: extendable_v[partial_embedding.size()][u_next]){
             partial_embedding.push_back(std::make_pair(u_next, v_next));
             visited_v[v_next] = true; visited_u[u_next] = true;
+            pushUV(partial_embedding.size(), u_next, v_next, data, query, cs);
             DoBacktrack(data, query, cs, partial_embedding);
+            popUV(u_next, query);
             partial_embedding.pop_back();
             visited_v[v_next] = false; visited_u[u_next] = false;
         }
-        /*Vertex root = partial_embedding.back().first + 1;
-        for(size_t idx = 0; idx < cs.GetCandidateSize(root); idx++){
-            Vertex next_vertex = cs.GetCandidate(root, idx);
-            partial_embedding.push_back(std::make_pair(root, next_vertex));
-            visited_v[next_vertex] = true; visited_u[root] = true;
-            DoBacktrack(data, query, cs, partial_embedding);
-            partial_embedding.pop_back();
-            visited_v[next_vertex] = false; visited_u[root] = false;
-        }*/
     }
 }
 
 Vertex Backtrack::FindRoot(const Graph& query, const CandidateSet& cs) {
     Vertex root = 0;
     //size_t min_neighbors = query.GetDegree(0);
-    size_t min_neighbors = cs.GetCandidateSize(0);
+    size_t min_candidate = cs.GetCandidateSize(0);
     for(size_t v=1;v<query.GetNumVertices();v++) {
-        if(cs.GetCandidateSize(v) < min_neighbors) {
+        //std::cout << "[CS SIZE] " << v << " " << cs.GetCandidateSize(v) << std::endl;
+        if(cs.GetCandidateSize(v) < min_candidate) { // Find Root that have most candidates in query
             root = v;
-            min_neighbors = cs.GetCandidateSize(v);
+            min_candidate = cs.GetCandidateSize(v);
         }
     }
     return Vertex(root);
 }
 
-Cmu& Backtrack::FindNextVertex(const Graph& data, const Graph& query, const CandidateSet& cs, std::vector<VertexPair>& partial_embedding, Cmu& cmu_next) {
-    // Not using DAG
-    std::set<Vertex> cmu;
-    std::set<Vertex> neighbors;
-    std::vector<Vertex> not_connected;
-    Vertex embedded_v;
-
-    size_t theta = size_t(query.GetNumVertices() - log2(query.GetNumVertices()));
-
-    // q means query.size() && p means partial_embedding.size()
-    // If p < q - log2(q) find neighbor by adding set
-    // time complexity : q log(q)
-    if(partial_embedding.size() <= theta) {
-        for(VertexPair p : partial_embedding) {
-            neighbors = query.GetNeighborSets(neighbors, p.first);
+Vertex Backtrack::FindNextVertex(size_t level) {
+    Vertex returnVertex = 99999;
+    size_t min_extendable = 10000;
+    for(int u=0;u<query_size;u++) {
+        if(extendable_u[level][u]) {
+            if(extendable_v[level][u].size() < min_extendable) {
+                returnVertex = u;
+                min_extendable = extendable_v[level][u].size();
+            }
         }
     }
-    // If p > q - log2(q) finding neighbor by comparing every member in partial_embedding
-    // time complexity : (q-p) * p
-    else {
-        for(size_t u=0;u<query.GetNumVertices();u++) {
-            if(visited_u[u]) continue;
-            for(auto p : partial_embedding) {
-                if(query.IsNeighbor(p.first, u)) {
-                    neighbors.insert(u);
-                    break;
+    return returnVertex;
+}
+
+/**
+ * @brief Returns the number of neighbors of v with label l.
+ *
+ * @param level-1 current extendable database.
+ * @param u, v partial_embedding에 이제 집어널을 녀석.
+ * @return extendable[level] u, v가 추가되었을 때 extendable database.
+ */
+void Backtrack::pushUV(size_t level, Vertex u, Vertex v, const Graph& data, const Graph& query, const CandidateSet& cs) {
+    // 추가할 u의 이웃을 불러온다 inssaPower[u] = 0;
+    // 이미 방문한 이웃 u_extend는 건너뛴다
+    // u의 이웃을 읽을 때 인싸력을 올려준다 i.e. inssaPower[u_iterate]++;
+    // 방문하지 않은 이웃에 대해
+    // u_extend의 c_M(u)를 불러온다
+    // partial_embedding을 읽으면서 지움
+    /*std::set<Vertex> neighbor;
+    neighbor = query.GetNeighborSets(neighbor, u);
+    for(Vertex u_neighbor : neighbor) {
+        inssaPower[u_neighbor]++;
+        //extendable_v[level+1][u_neighbor].clear();
+        if(!visited_u[u_neighbor]) {
+            extendable_u[level+1][u_neighbor] = true;
+            for(Vertex v_cmu : extendable_v[level][u_neighbor]) {
+                for(VertexPair p : partial_embedding) {
+                    if(!visited_v[v_cmu] && (!query.IsNeighbor(p.first, u_neighbor) || data.IsNeighbor(p.second, v_cmu))) {
+                        extendable_v[level+1][u_neighbor].insert(v_cmu);
+                        break;
+                    }
                 }
             }
         }
-    }
-
-    /*std::cout << "[Neighbors]";
-    for(Vertex u : neighbors) std::cout << " " << u;
-    std::cout << std::endl;*/
-
-    for(Vertex u : neighbors) {
-        if(visited_u[u]) continue;
-        cmu.clear();
-        cmu = cs.GetCandidateSet(cmu, u);
-        /*std::cout << "[b_B(k)] " << u << " " << cmu.size();
-        for(Vertex v : cmu) std::cout << " " << v;
-        std::cout << std::endl;*/
-        for(VertexPair p : partial_embedding) {
-            not_connected.clear();
-            if(!query.IsNeighbor(p.first, u)) continue;
-            if(cmu.size() == 0) break;
-            embedded_v = p.second;
-            for(Vertex curr_v : cmu) {
-                if(visited_v[curr_v] || !data.IsNeighbor(embedded_v, curr_v)) not_connected.push_back(curr_v);
-                /*if(visited_v[curr_v]) {
-                    std::cout << "[NOT_CONNECTED] " << curr_v << " is visited" << std::endl;
-                }
-                if(!data.IsNeighbor(embedded_v, curr_v)) {
-                    std::cout << "[NOT_CONNECTED] " << embedded_v << "," << curr_v << " isn't connected while "
-                                << p.first << "," << u << " is connected" << std::endl;
-                }*/
+    }*/
+    for(int w=0;w<query_size;w++) {
+        extendable_v[level+1][w].clear();
+        // w == u : 할게 없다
+        // w가 visited면 : 할게 없다
+        // w가 extendable_u[level][w] : extendable_v[level][w]에서 for loop 돌리면서 채우기(드모르간)
+        // w가 신유형이면 : getCandidateSet() 불러와서 여기서 for loop 돌리기
+        if(visited_u[w] || w == u) continue;
+        if(extendable_u[level][w]) {
+            extendable_u[level+1][w] = true;
+            for(Vertex v_cmu : extendable_v[level][w]) {
+                if(!query.IsNeighbor(w,u) || data.IsNeighbor(v_cmu,v)) extendable_v[level+1][w].insert(v_cmu);
             }
-            for(Vertex v: not_connected) cmu.erase(v);
-        }
-
-        // Find possible u that has minimum |C_m(u)|
-        /*std::cout << "[c_M(u)] " << u << " " << cmu.size();
-        for(Vertex v : cmu) std::cout << " " << v;
-        std::cout << std::endl;*/
-        if(!cmu.empty()) {
-            if(cmu_next.second.size() == 0) {
-                cmu_next = std::make_pair(u,cmu);
-            }
-            if(cmu.size() == 1) {
-                cmu_next.first = u;
-                cmu_next.second = cmu;
-                break;
-            }
-            // CHANGE HERE TO CHANGE CRITERION!!!!!!!!!!
-            if(cmu.size() < cmu_next.second.size()) {
-                cmu_next.first = u;
-                cmu_next.second = cmu;
+        } else if(query.IsNeighbor(w,u)) {
+            extendable_u[level+1][w] = true;
+            std::set<Vertex> candidate_set;
+            candidate_set = cs.GetCandidateSet(candidate_set, w);
+            for(Vertex v_cmu : candidate_set) {
+                if(!query.IsNeighbor(w,u) || data.IsNeighbor(v_cmu,v)) extendable_v[level+1][w].insert(v_cmu);
             }
         }
     }
+}
 
-    return cmu_next;
+void Backtrack::popUV(Vertex u, const Graph& query) {
+    // inssaPower만 관리해줌;
+    std::set<Vertex> neighbor;
+    neighbor = query.GetNeighborSets(neighbor, u);
+    for(Vertex u_neighbor : neighbor) {
+        inssaPower[u_neighbor]--;
+    }
 }
 
 void Backtrack::Check(const Graph& data, const Graph& query, const CandidateSet& cs, std::vector<VertexPair>& partial_embedding) {
@@ -220,6 +205,7 @@ void Backtrack::Check(const Graph& data, const Graph& query, const CandidateSet&
         }
     }
     std::cout << "[T] ";
+    //PrintPath(partial_embedding);
 }
 
 Vertex Backtrack::FindVByU(std::vector<VertexPair>& partial_embedding, Vertex u){
